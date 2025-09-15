ca177a2c4c70dda80b96173f93ef0d364e03997a
Yuki Sakai <43900255+VeyronSakai@users.noreply.github.com>
2025-09-15 15:21:54 +0900

    refactor: add AAA comments and clean up GitRepositoryStub
    - Add Arrange, Act, Assert comments to conflictAnalyzer tests
    - Remove unused methods from GitRepositoryStub (getCommits, reset, createWithSampleConflicts)
    - Remove unused ConflictType import from GitRepositoryStub
    - Keep only necessary test helper methods

--- __tests__/test-doubles/gitRepositoryStub.ts
+++ __tests__/test-doubles/gitRepositoryStub.ts
@@ -1,13 +1,11 @@
 import { GitRepository } from '@domains/repositories/gitRepository.js'
 import { ConflictedFile } from '@domains/entities/conflictedFile.js'
-import { ConflictType } from '@domains/value-objects/conflictType.js'
 import { ResolutionStrategy } from '@domains/value-objects/resolutionStrategy.js'
 
 export class GitRepositoryStub implements GitRepository {
   private conflictedFiles: ConflictedFile[]
   private resolvedFiles: Map<string, ResolutionStrategy> = new Map()
   private stagedFiles: Set<string> = new Set()
-  private commits: string[] = []
 
   constructor(conflictedFiles: ConflictedFile[] = []) {
     this.conflictedFiles = conflictedFiles
@@ -29,7 +27,7 @@ export class GitRepositoryStub implements GitRepository {
   }
 
   async commitChanges(message: string): Promise<void> {
-    this.commits.push(message)
+    // Required by GitRepository interface but not used in tests
   }
 
   getResolvedFiles(): Map<string, ResolutionStrategy> {
@@ -38,25 +36,5 @@ export class GitRepositoryStub implements GitRepository {
 
   getStagedFiles(): Set<string> {
     return this.stagedFiles
-  }
-
-  getCommits(): string[] {
-    return this.commits
-  }
-
-  reset(): void {
-    this.conflictedFiles = []
-    this.resolvedFiles.clear()
-    this.stagedFiles.clear()
-    this.commits = []
-  }
-
-  static createWithSampleConflicts(): GitRepositoryStub {
-    return new GitRepositoryStub([
-      { path: 'package-lock.json', conflictType: ConflictType.BothModified },
-      { path: 'src/index.ts', conflictType: ConflictType.BothModified },
-      { path: 'config.generated.ts', conflictType: ConflictType.BothModified },
-      { path: 'deleted-file.ts', conflictType: ConflictType.DeletedByUs }
-    ])
   }
 }
